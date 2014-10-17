610bc51bba867f893e483870ce88a51cb31a3b09
scrbonilla <scrbonilla84@gmail.com>
2014-10-17 00:47:08 -0400

    Search in Blog fails Multiple error now

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ActiveRecord::Base
+
     scope :most_recent, -> {limit(10).order('published_at desc')}
+
+    validates :subject, presence: true, uniqueness: true
+    validates :body, presence: true, uniqueness: true
+
     def self.search(query)
         where("subject like ?", "%#{query}%")
     end
